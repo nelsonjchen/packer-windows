bd3cb5fce464bbfa16c1cc3e369e800a3067522d
Nelson Chen <crazysim@gmail.com>
2018-09-05 00:41:06 -0700

    Use aria2c

--- virtio.sh
+++ virtio.sh
@@ -2,4 +2,4 @@
 export VIRTIO_WIN_ISO_URL=${VIRTIO_WIN_ISO_URL:-https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/latest-virtio/virtio-win.iso}
 export VIRTIO_WIN_ISO=${VIRTIO_WIN_ISO:-iso/$(basename $VIRTIO_WIN_ISO_URL)}
 
-test -f $VIRTIO_WIN_ISO || wget --continue $VIRTIO_WIN_ISO_URL -O $VIRTIO_WIN_ISO
+test -f $VIRTIO_WIN_ISO || aria2c $VIRTIO_WIN_ISO_URL -o $VIRTIO_WIN_ISO
